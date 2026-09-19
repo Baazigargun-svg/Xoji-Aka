670aba520469184f0e20f6c136c3132cfa15e671
Baazigargun-svg <Baazigargun@mail.ru>
2026-09-20 00:36:13 +0500

    Update run.py

--- run.py
+++ run.py
@@ -1,11 +1,1 @@
-import subprocess
-import sys
 
-# 1. Telegram botni ishga tushirish
-bot_process = subprocess.Popen([sys.executable, "main.py"])
-
-# 2. Veb-saytni (Flask) ishga tushirish
-try:
-    subprocess.run([sys.executable, "app.py"])
-except KeyboardInterrupt:
-    bot_process.terminate()
